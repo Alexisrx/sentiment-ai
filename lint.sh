f8e6262c4304067cd88f8246d1c3b36909fb9bdb
Alexis ROUX <alexisroux2003@gmail.com>
2026-06-18 11:46:50 +0200

    fix: correct lint.sh content

--- lint.sh
+++ lint.sh
@@ -1,11 +1,3 @@
-stage('Lint') {
-            steps {
-                sh '''
-                    docker run --rm \
-                    -v $WORKSPACE:/app \
-                    -w /app \
-                    python:3.12-slim \
-                    sh lint.sh
-                '''
-            }
-        }
+﻿#!/bin/sh
+pip install flake8 -q
+flake8 src/ --max-line-length=100
